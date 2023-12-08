8275f5b9b01bf7fb540387fa5e0d95239658574c
Zachary Wallace <zwall@osuosl.org>
2023-12-08 15:27:05 -0800

    Initial unit test failing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,6 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ALMALINUX_8 = {
-  platform: 'almalinux',
-  version: '8',
-}.freeze
-
-ALL_PLATFORMS = [
-  ALMALINUX_8,
-].freeze
-
 RSpec.configure do |config|
   config.log_level = :warn
 end
--- spec/unit/resources/osl_element_spec.rb
+++ spec/unit/resources/osl_element_spec.rb
@@ -7,22 +7,33 @@
 
 require_relative '../../spec_helper'
 
-describe 'create_element_site' do
-  platform 'almalinux'
+describe 'osl_element' do
+  platform 'almalinux', '8'
   step_into :osl_element
 
-  recipe do
-    osl_element 'chat.example.org'
-  end
+  context 'Create' do
+    cached(:subject) { chef_run }
 
-  it { is_expected.to include_recipe('osl-nginx') }
+    recipe do
+      osl_element 'chat.example.org'
+    end
 
-  describe run_docker_container('Element Webapp') do
-    its('repo') { should eq 'vectorim/element-web' }
-    its('exposed_ports') { should eq({ '80' => '80' }) }
-  end
+    it { is_expected.to create_directory('/opt/element') }
 
-  describe create_nginx_app('Element Frontend') do
-    its('cookbook') { should eq 'osl-matrix' }
+    it do
+      is_expected.to create_template('/opt/element/config.json').with(
+        source: 'element-config.json.erb'
+      )
+    end
+
+    it { is_expected.to pull_docker_image('vectorim/element-web') }
+
+    it do
+      is_expected.to run_docker_container('element_webapp').with(
+        repo: 'vectorim/element-web',
+        exposed_ports: { '80' => '80' },
+        volumes: ['/opt/element/config.json:/app/config.json']
+      )
+    end
   end
 end
