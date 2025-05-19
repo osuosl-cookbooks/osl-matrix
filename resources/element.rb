f8a38be98efa5adca21b9b0bab5effff1a22cc69
Zachary Wallace <zwall@osuosl.org>
2025-05-19 15:04:12 -0700

    Added branding support to Element

--- resources/element.rb
+++ resources/element.rb
@@ -9,18 +9,39 @@ default_action :create
 
 property :matrix_domain, String, name_property: true
 property :port, Integer, default: 8000
+property :background, String
+property :logo, String
 
 action :create do
   include_recipe 'osl-docker' do
     notifies :restart, 'docker_container[element_webapp]'
   end
 
+  volumes = ['/opt/element/config.json:/app/config.json:ro']
+
   directory '/opt/element'
+
+  # Custom branding
+  enable_branding = new_resource.background || new_resource.logo
+  
+  cookbook_file "/opt/element/background#{::File.extname(new_resource.background)}" do
+    source new_resource.background
+    only_if { new_resource.background }
+  end
+
+  volumes.push("/opt/element/background#{::File.extname(new_resource.background)}:/app/assets/background#{::File.extname(new_resource.background)}:ro") if new_resource.background
+
+  cookbook_file "/opt/element/logo#{::File.extname(new_resource.logo)}" do
+    source new_resource.logo
+    only_if { new_resource.logo }
+  end
+
+  volumes.push("/opt/element/logo#{::File.extname(new_resource.logo)}:/app/assets/logo#{::File.extname(new_resource.logo)}:ro") if new_resource.logo
 
   template '/opt/element/config.json' do
     source 'element-config.json.erb'
     cookbook 'osl-matrix'
-    variables(fqdn: new_resource.matrix_domain)
+    variables(fqdn: new_resource.matrix_domain, branding: enable_branding)
     sensitive true
     notifies :redeploy, 'docker_container[element_webapp]'
   end
@@ -33,6 +54,6 @@ action :create do
   docker_container 'element_webapp' do
     repo 'vectorim/element-web'
     port ["#{new_resource.port}:80"]
-    volumes ['/opt/element/config.json:/app/config.json']
+    volumes volumes
   end
 end
